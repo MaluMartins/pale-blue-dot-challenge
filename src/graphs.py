3a317c1703a8b54e159ab61b3dfddfec4adb8b55
MaluMartis <martinss.mmalu@gmail.com>
2023-12-13 01:57:57 -0300

    style: style fonts and charts

--- src/graphs.py
+++ src/graphs.py
@@ -14,16 +14,17 @@ file_ch4 = "ch4"
 file_co2 = "co2"
 file_o3 = "o3"
 
-def create_chart(path, y_name, html_file):
+def create_chart(path, y_name, html_file, chart_title):
     data = pd.read_csv(path, skiprows=9,  usecols=[0,1], names=['Time',f'{y_name}'])
 
     # Read the times as dates and the methane concentrations as numeric values
     data['Time'] = pd.to_datetime(data['Time'])
     data[f'{y_name}'] = pd.to_numeric(data[f'{y_name}'])
 
-    figCH4 = px.line(data, x="Time", y=f'{y_name}')
-    figCH4.write_html(f"C:/pale-blue-dot-challenge/src/figures/{html_file}.html")
+    fig = px.line(data, x="Time", y=f'{y_name}', title=f'{chart_title}')
+    fig.update_layout(title_x=0.5)
+    fig.write_html(f"C:/pale-blue-dot-challenge/src/figures/{html_file}.html")
 
-# create_chart(path_ch4_data, y_name_ch4, file_ch4)
-create_chart(path_co2_data, y_name_co2, file_co2)
-create_chart(path_ozone_data, y_name_o3, file_o3)
+create_chart(path_ch4_data, y_name_ch4, file_ch4, "Methane")
+create_chart(path_co2_data, y_name_co2, file_co2, "Carbon Dioxide")
+create_chart(path_ozone_data, y_name_o3, file_o3, "Ozone")
